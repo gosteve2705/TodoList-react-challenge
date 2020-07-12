72bf230a1ce5a28414c1a72f3bb73df8d1646e08
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-07-12 10:31:27 +0100

    added states
    to track and update items

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 
 function App() {
+  
+  const [input, setInput] = useState("");
+  const [items,setItems] = useState([]);
+  function handleChange(event) {
+    const newValue = event.target.value;
+    
+    setInput(newValue);
+    
+    
+  }
+  function addItems(event) {
+   setItems(prevValue=>{
+     return [...prevValue,input]
+   });
+    event.preventDefault();
+    setInput('');
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
+        <form onSubmit={addItems}>
+          <input onChange={handleChange} type="text" value={input} />
+          <button type="submit">
+            <span>Add</span>
+          </button>
+        </form>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {items.map(item => {
+            return <li>{item} </li>;
+          })}
+          
         </ul>
       </div>
     </div>
   );
 }
+
 
 export default App;
